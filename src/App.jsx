376f48255be53cd83cf95debd04b4d35e49c126e
Gokul-HP <gvarutharaj@gmail.com>
2023-12-14 21:16:13 +0530

    nav 1st commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
 import './App.css'
+import Navbar from './navBar/Navbar'
 
 function App() {
 
   return (
     <div className='app'>
-      <div className='nav'>
-        <div className='logo'>
-          <img className='logo-svg' src="src\assets\logo.svg" alt="flapkart logo" />
-        </div>
-        <div className='search-bar'>
-          <img className='search-icon' src="src\assets\icons-search.svg" alt="" />
-          <input className='search' type="search" name="" id="" placeholder='Search for products, brands etc.,.' />
-        </div>
-        <div>
-          
-        </div>
-        <div>cart</div>
-        <div>Seller</div>
-        <div>dropdown</div>
-      </div>
+      <Navbar></Navbar>
+
       <div className='category'>product category</div>
       <div className='offers'>offers</div>
+
     </div>
   )
 }
---
+++ src/imgSlider/Slider.jsx
@@ -0,0 +1,11 @@
+import './Slider.css'
+
+const Slider = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Slider
