2d26778142045512927328ce6d79b6509f5ff880
Gokul-HP <gvarutharaj@gmail.com>
2024-01-08 12:05:06 +0530

    update1

--- src/category/Category.jsx
+++ src/category/Category.jsx
@@ -14,7 +14,9 @@ import SubCategory from './sub-category/SubCategory'
 
 const Category = () => {
 
-    const [showTypes, setShowTypes] = useState(true)
+    const [showTypes, setShowTypes] = useState({ display: "none", left: 0 })
+
+    // const [showTypes, setShowTypes]
 
     const categoryImgs = [
         { name: 'Offers', path: offers },
@@ -29,22 +31,107 @@ const Category = () => {
         { name: 'Mobiles', path: mobile }
     ];
 
+
     return (
         <>
-            <div className='category-con'>
+            {/* <div className='category-con'>
                 {categoryImgs.map((imgs) => {
                     return <div
                         onMouseEnter={() => setShowTypes(true)}
+                        onMouseLeave={() => { setShowTypes(false) }}
                         className='category-item' key={imgs.name}>
 
                         <img src={imgs.path} />
                         <p>{imgs.name}</p>
                     </div>
                 })}
+                {showTypes && <SubCategory></SubCategory>}
+            </div> */}
+
+            {/* <div className='category-con'>
+                {categoryImgs.map((imgs) => {
+                    return <div
+                        className='category-main-item'
+                        key={imgs.name}>
+
+                        <div className='category-item'
+                            onMouseEnter={() => setShowTypes(true)}
+                            onMouseLeave={() => setShowTypes(false)}>
+                            <img src={imgs.path} />
+                            <p>{imgs.name}</p>
+                        </div>
+                    </div>
+                })}
+                {showTypes && <SubCategory></SubCategory>}
+            </div> */}
+
+            {/* <div className='category-con'>
+                {categoryImgs.map((imgs) => {
+                    return <div
+                        // className='category-main-item'
+                        onMouseEnter={(e) => {
+                            setShowTypes({ display: "flex", left: e.screenX});
+                            console.log(e);
+                        }}
+                        onMouseLeave={(e) => setShowTypes({ display: "none", left: e.target.offsetLeft })}
+
+                        key={imgs.name}>
+
+                        <div className='category-item'>
+                            <img src={imgs.path} />
+                            <p>{imgs.name}</p>
+                        </div>
+                    </div>
+                })}
+                {<div style={{ position: "absolute", display: `${showTypes.display}`, left: `${showTypes.left}px` }}>
+                    <SubCategory ></SubCategory>
+                </div>}
+            </div> */}
+
+            {/* <div className='category-con'>
+                {categoryImgs.map((imgs) => {
+                    return <div
+                        // className='category-main-item'
+                        onMouseEnter={(e) => {
+                            setShowTypes({ display: "flex", left: e.clientX });
+                            console.log(e);
+                        }}
+                        onMouseLeave={(e) => setShowTypes({ display: "none", left: e.target.offsetLeft })}
+
+                        key={imgs.name}>
+
+                        <div className='category-item'>
+                            <img src={imgs.path} />
+                            <p>{imgs.name}</p>
+                        </div>
+                        {<div style={{ position: "absolute", display: `${showTypes.display}`, left: `${showTypes.left}px` }}>
+                            <SubCategory></SubCategory>
+                        </div>}
+                    </div>
+                })}
+            </div> */}
+
+            <div className='category-con'>
+                {categoryImgs.map((imgs) => {
+                    return <div
+                        onMouseEnter={(e) => {
+                            setShowTypes({ display: "flex", left: e.clientX });
+                            console.log(e);
+                        }}
+                        onMouseLeave={(e) => setShowTypes({ display: "none", left: e.target.offsetLeft })}
+
+                        key={imgs.name}>
+
+                        <div className='category-item'>
+                            <img src={imgs.path} />
+                            <p>{imgs.name}</p>
+                        </div>
+                        {<div style={{ position: "absolute", display: `${showTypes.display}`, left: `${showTypes.left}px` }}>
+                            <SubCategory></SubCategory>
+                        </div>}
+                    </div>
+                })}
             </div>
-
-            <SubCategory style={{ position: "absolute" }}></SubCategory>
-
         </>
     )
 }
